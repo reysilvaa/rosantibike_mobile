60eab8a1f5951edc7bbe0306927a9d0de01ef0a5
reysilvaa <reynaldsilva123@gmail.com>
2025-01-20 19:40:25 +0700

    fix error when token expired

--- lib/api/auth_api.dart
+++ lib/api/auth_api.dart
@@ -6,6 +6,7 @@ import '../api/api_service.dart';
 class AuthApi {
   final String baseUrl = ApiService.apiUrl;
 
+  // Login function
   Future<Map<String, dynamic>> login(String uname, String pass) async {
     try {
       final response = await http.post(
@@ -23,12 +24,20 @@ class AuthApi {
         if (data['access_token'] != null) {
           final prefs = await SharedPreferences.getInstance();
           await prefs.setString('access_token', data['access_token']);
+          await prefs.setInt(
+              'expires_in',
+              DateTime.now()
+                  .add(Duration(seconds: data['expires_in']))
+                  .millisecondsSinceEpoch);
           return {'success': true, 'access_token': data['access_token']};
         } else {
           return {'success': false, 'message': data['error']};
         }
       } else {
-        return {'success': false, 'message': 'Something went wrong. Please try again.'};
+        return {
+          'success': false,
+          'message': 'Something went wrong. Please try again.'
+        };
       }
     } catch (e) {
       throw Exception('Failed to login: $e');
@@ -37,6 +46,8 @@ class AuthApi {
 
   Future<bool> logout(String token) async {
     try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('access_token') ?? '';
       final response = await http.post(
         Uri.parse('$baseUrl/logout'),
         headers: {
@@ -46,8 +57,8 @@ class AuthApi {
       );
 
       if (response.statusCode == 200) {
-        final prefs = await SharedPreferences.getInstance();
         await prefs.remove('access_token');
+        await prefs.remove('expires_in');
         return true;
       } else {
         final data = jsonDecode(response.body);
@@ -56,5 +67,32 @@ class AuthApi {
     } catch (e) {
       throw Exception('Failed to logout: $e');
     }
+  }
+
+  // Check if token is expired
+  Future<bool> isTokenExpired() async {
+    final prefs = await SharedPreferences.getInstance();
+    final expiresIn = prefs.getInt('expires_in') ?? 0;
+    return DateTime.now().millisecondsSinceEpoch > expiresIn;
+  }
+
+  // Get a valid token
+  Future<String?> getValidToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('access_token');
+
+    if (token == null) {
+      throw Exception('Token not found. Please login.');
+    }
+
+    // Check if token is expired
+    final isExpired = await isTokenExpired();
+    if (isExpired) {
+      prefs.remove('access_token');
+      prefs.remove('expires_in');
+      throw Exception('Token expired. Please login again.');
+    }
+
+    return token; // Return valid token
   }
 }
